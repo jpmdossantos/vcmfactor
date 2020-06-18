c6fa8838fb71bac673c0334010235f95cec275a1
Joao Paulo Santos <joao.santos@diagno.ind.br>
2020-06-18 12:01:15 -0300

    plota erro

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -10,31 +10,19 @@ def data_load():
     df['Identifier'] = df.Serial + " " + df.Amostra.astype(str)
     return df
 
-def extract_factor(data):
+def extract_new_column(data, column, extra = 0):
     for id in data.Identifier.unique():
-        print(id)
         selected = data.Identifier == id
         base_vcm = data.loc[(selected) & (data.Ganho==30), 'VCM'].astype(float).values
-        data.loc[selected,'Fator VCM'] =  base_vcm / data.loc[selected,'VCM'] 
-        
-def plot_raw(data):
-    sns.set_style("darkgrid")
-    plt.figure(figsize=(12,6))   
-
-    sns.scatterplot(x='Ganho', y='Fator VCM',data=data,hue='Identifier')
-    plt.legend(loc='upper right')
-    
-    sns.lineplot(x='Ganho', y='Fator VCM',data=data)
-    
-#   sns.lmplot(x='Ganho', y='Fator VCM',data=data,hue='Identifier',size=(7),aspect=(12.0/7),legend=False)
-#   plt.legend(loc='upper left')
-
-    plt.show()
+        if column == "factor":
+            data.loc[selected,'Fator VCM'] =  base_vcm / data.loc[selected,'VCM'] 
+        elif column == "error":
+            data.loc[selected,'Error']=calculate_error(data.loc[selected], extra, base_vcm)
     
 def bootstrap(array):
-    B_repeats = 100
+    B_repeats = 10000
     replicates = []
-    for _ in range(1000):
+    for _ in range(B_repeats):
         boot_factor = np.random.choice(array,len(array))
         m = np.median(boot_factor)
         replicates.append(m)
@@ -55,22 +43,70 @@ def create_samples_bootstrap(df):
         ylower.append(bootstrapped[0])
         yupper.append(bootstrapped[2])
         
-    plot_bootstrap(x,y,ylower,yupper)
+    result = pd.DataFrame({'Ganho': x, 'Central': y,
+                           'Lower': ylower, 'Upper':yupper}) 
+    return(result)
 
-def plot_bootstrap(x, central, lower, upper):
-    plt.plot(x,central)
-    plt.fill_between(x,lower,upper,alpha=.5)
+def plot_bootstrap(boot_df):
+    plt.plot(boot_df.Ganho,boot_df.Central)
+    plt.fill_between(boot_df.Ganho,boot_df.Lower,boot_df.Upper,alpha=.5)
     plt.show()
 
-bs = False     
+def plot_raw(data, xname, yname):
+    sns.set_style("darkgrid")
+    plt.figure(figsize=(12,6))   
+    sns.scatterplot(x=xname, y=yname, data=data,hue='Identifier')
+    plt.legend(loc='upper right')
+    
+    sns.lineplot(x=xname, y=yname,data=data)
+    
+#   sns.lmplot(x='Ganho', y='Fator VCM',data=data,hue='Identifier',size=(7),aspect=(12.0/7),legend=False)
+#   plt.legend(loc='upper left')
+
+    plt.show()
+
+def plot_error(data, xname, yname):  
+    sns.set_style("darkgrid")
+    plt.figure(figsize=(12,6))   
+    plt.ylim(-0.5,6)
+    #for iden in data.Serial.unique():
+    #    selected = data.loc[data.Serial==iden]
+    #    sns.lineplot(x=xname, y=yname,data=selected)
+            
+    sns.scatterplot(x=xname, y=yname, data=data)#,hue='Serial')
+    ax = sns.lineplot(x=xname, y=yname,data=data,color='r')
+    plt.title("Erro médio absoluto do VCM obtido utilizando os fatores por ganho")
+    ax.set(xlabel='Ganho', ylabel='Erro (fL)')
+    plt.legend(loc='upper left')
+    plt.show()
+    
+
+def calculate_error(df, boot_result, base_vcm):
+    errors = []
+    for index, row in df.iterrows():
+        error = boot_result.loc[boot_result.Ganho==row.Ganho,'Central']*row.VCM - base_vcm
+        errors.append(abs(error.values[0]))
+    error_series = pd.Series(errors, dtype=float, index=df.index)
+
+    return error_series
+        
+        
+    
+    
+
+bs = True
     
 measures = data_load()
-measures.dropna()
-extract_factor(measures)
+measures = measures.dropna()
+extract_new_column(measures, "factor")
 if bs:
-    create_samples_bootstrap(measures)
+    boot_df = create_samples_bootstrap(measures)
+#    plot_bootstrap(boot_df)
+    extract_new_column(measures, "error", boot_df)
+    plot_error(measures,'Ganho','Error')
+    
 else:
-    plot_raw(measures)
+    plot_raw(measures,'Ganho', 'Fator VCM')
 
 
 #sns.scatterplot(x=data.a, y=data.w)
